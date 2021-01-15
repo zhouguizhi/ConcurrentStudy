4e5d729cd75526e80bc1f722a799fd5db963b29f
zhou.gz <2327322601@qq.com>
2021-01-15 13:47:51 +0800

    删除无用的代码

--- src/threadcoreknowledge/objectcommonmethods/WaitNotifyPrintOddEvenSyn.java
+++ src/threadcoreknowledge/objectcommonmethods/WaitNotifyPrintOddEvenSyn.java
@@ -6,14 +6,10 @@ package threadcoreknowledge.objectcommonmethods;
  * 所以最好还是改用 wait,notify实现
  */
 public class WaitNotifyPrintOddEvenSyn {
-
     private static int count;
     private static int max_count = 100;
     private static final Object lock = new Object();
-
-    //新建2个线程
-    //1个只处理偶数，第二个只处理奇数（用位运算）
-    //用synchronized来通信
+    
     public static void main(String[] args) {
         new Thread(new Runnable() {
             @Override
