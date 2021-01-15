25aa2bc3b2137d942e64f4449f706a405f08e832
zhou.gz <2327322601@qq.com>
2021-01-15 13:45:28 +0800

    添加备注

--- src/threadcoreknowledge/objectcommonmethods/WaitNotifyPrintOddEvenSyn.java
+++ src/threadcoreknowledge/objectcommonmethods/WaitNotifyPrintOddEvenSyn.java
@@ -2,6 +2,8 @@ package threadcoreknowledge.objectcommonmethods;
 /**
  * 描述：两个线程交替打印0~100的奇偶数，用synchronized关键字实现
  * 在这学到了一点怎么判断奇偶数  n&1=0表示偶数 n&1=1表示奇数
+ * 这个是实现方案不是最好的方案,因为会出现当count为偶数或者奇数,它会执行奇数线程,偶数线程,会造成cpu的浪费,虽然功能上能实现
+ * 所以最好还是改用 wait,notify实现
  */
 public class WaitNotifyPrintOddEvenSyn {
 
