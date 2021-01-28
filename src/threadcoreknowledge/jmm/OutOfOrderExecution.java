58b8df7a4b4c78766a5f722e619ff43cf7f97235
zhou.gz <2327322601@qq.com>
2021-01-28 17:32:14 +0800

    演示重排序出现的问题

--- src/threadcoreknowledge/jmm/OutOfOrderExecution.java
+++ src/threadcoreknowledge/jmm/OutOfOrderExecution.java
@@ -8,6 +8,7 @@ import java.util.concurrent.CountDownLatch;
  * 1:线程1先运行 线程2后运行,结果是x=0,y=1
  * 2:线程2先运行,线程1后运行,x=1,y=0 ,让线程2先执行start()就很容易出现
  * 3:线程1和线程2都统一先执行到第一步,然后再执行第二行代码,a=1,b=1,然后再执行x = b  y=a,这个时候就是x=1,y=1了
+ * 4:第四种情况就是x=0,y=0了 这个是在什么场景下发生的呢？
  */
 public class OutOfOrderExecution {
     private static int x,y=0;
@@ -53,7 +54,15 @@ public class OutOfOrderExecution {
             thread1.join();
             thread2.join();
             String result = "第"+i+"次("+"x="+x+","+"y="+y+")";
-            if(x==1&&y==1){
+            
+//            if(x==1&&y==1){
+//                System.out.println(result);
+//                break;
+//            }else{
+//                System.out.println(result);
+//            }
+            //这是演示重排序的问题
+            if(x==0&&y==0){
                 System.out.println(result);
                 break;
             }else{
